11e89511d731b23fbd83f32c78268cb4d7ff4410
トリナー <toliner.rkt@gmail.com>
2018-12-01 19:35:51 +0900

    add dependence on kotlinx.atomicfu

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val atomicfu_version: String by project
+
+buildscript {
+    val atomicfu_version: String by project
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:$atomicfu_version")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.10"
+}
+
+apply {
+    plugin("kotlinx-atomicfu")
 }
 
 group = "toliner.utils"
@@ -14,6 +27,8 @@ repositories {
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
+    compileOnly("org.jetbrains.kotlinx:atomicfu:${atomicfu_version}")
+    testRuntime("org.jetbrains.kotlinx:atomicfu:${atomicfu_version}")
 }
 
 tasks.withType<KotlinCompile> {
